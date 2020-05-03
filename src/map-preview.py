10912a71e12c29b479d33da9195d6e84a39bf159
Dan O'Neill <dev@jdoneill.com>
2020-05-02 19:45:42 -0700

    valid json check

--- src/map-preview.py
+++ src/map-preview.py
@@ -1,6 +1,7 @@
 # -*- encoding: UTF-8 -*-
 import codecs
 import os
+import json
 import sublime
 import sublime_plugin
 import tempfile
@@ -37,45 +38,50 @@ class PreviewCommand(sublime_plugin.TextCommand):
         view = self.view
         geojson = view.substr(sublime.Region(0, view.size()))
 
-        messageBegin = """
-          <html>
-            <head>
-              <title>ST-Map Preview</title>
-              <link rel="stylesheet" href="https://unpkg.com/leaflet@1.6.0/dist/leaflet.css"
-                integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ=="
-                crossorigin=""/>
-              <script src="https://unpkg.com/leaflet@1.6.0/dist/leaflet.js"
-                integrity="sha512-gZwIG9x3wUXg2hdXF6+rVkLF/0Vi9U8D2Ntg4Ga5I5BZpVkVxlJWbSQtXPSiUTtC0TjtGOmxa1AJPuV0CPthew=="
-                crossorigin=""></script>
-              <style>
-                #map{ height: 100% }
-              </style>
+        try:
+            json.loads(geojson)
+
+            messageBegin = """
+            <html>
+              <head>
+                <title>ST-Map Preview</title>
+                <link rel="stylesheet" href="https://unpkg.com/leaflet@1.6.0/dist/leaflet.css"
+                  integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ=="
+                  crossorigin=""/>
+                <script src="https://unpkg.com/leaflet@1.6.0/dist/leaflet.js"
+                  integrity="sha512-gZwIG9x3wUXg2hdXF6+rVkLF/0Vi9U8D2Ntg4Ga5I5BZpVkVxlJWbSQtXPSiUTtC0TjtGOmxa1AJPuV0CPthew=="
+                  crossorigin=""></script>
+                <style>
+                  #map{ height: 100% }
+                </style>
+              </head>
+              <body>
+                <div id="map"></div>
+                <script>
+                  mapLink = '<a href="http://openstreetmap.org">OpenStreetMap</a>';
+                  var tiles = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+                  attribution: '&copy; ' + mapLink + ' Contributors'
+                 });
+            """
+
+            geojsonFeature = "    var geojsonFeature = {};".format(geojson)
+
+            messageEnd = """
+                  var map = L.map('map');
+                  tiles.addTo(map);
+                  L.geoJSON(geojsonFeature).addTo(map);
+                  map.fitBounds(L.geoJSON(geojsonFeature).getBounds())
+                </script>
+              </body>
             </head>
-            <body>
-              <div id="map"></div>
-              <script>
-                mapLink = '<a href="http://openstreetmap.org">OpenStreetMap</a>';
-                var tiles = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-                attribution: '&copy; ' + mapLink + ' Contributors'
-               });
-        """
+            """
 
-        geojsonFeature = "    var geojsonFeature = {};".format(geojson)
+            html = messageBegin + geojsonFeature + messageEnd
 
-        messageEnd = """
-              var map = L.map('map');
-              tiles.addTo(map);
-              L.geoJSON(geojsonFeature).addTo(map);
-              map.fitBounds(L.geoJSON(geojsonFeature).getBounds())
-            </script>
-          </body>
-        </head>
-        """
+            # update output HTML file
+            tmp_fullpath = get_temp_preview_path(view)
+            save_utf8(tmp_fullpath, html)
 
-        html = messageBegin + geojsonFeature + messageEnd
-
-        # update output HTML file
-        tmp_fullpath = get_temp_preview_path(view)
-        save_utf8(tmp_fullpath, html)
-
-        webbrowser.open_new_tab("file:///" + tmp_fullpath)
+            webbrowser.open_new_tab("file:///" + tmp_fullpath)
+        except Exception as ex:
+            sublime.error_message("Could not convert file.\n\n %s" % ex)
