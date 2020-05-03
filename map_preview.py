44ffb7d25394c81853f3390f87d902752e8987f6
Dan O'Neill <dev@jdoneill.com>
2020-05-03 14:36:14 -0700

    check if valid geojson

--- map_preview.py
+++ map_preview.py
@@ -43,67 +43,71 @@ class PreviewCommand(sublime_plugin.TextCommand):
             validateJson = json.loads(viewText)
             # check if json valid spatial type
             if "type" in validateJson:
-                messageBegin = """
-                <html>
-                  <head>
-                    <title>ST-Map Preview</title>
-                    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.6.0/dist/leaflet.css"
-                      integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ=="
-                      crossorigin=""/>
-                    <script src="https://unpkg.com/leaflet@1.6.0/dist/leaflet.js"
-                      integrity="sha512-gZwIG9x3wUXg2hdXF6+rVkLF/0Vi9U8D2Ntg4Ga5I5BZpVkVxlJWbSQtXPSiUTtC0TjtGOmxa1AJPuV0CPthew=="
-                      crossorigin=""></script>
-                    <style>
-                      #map{ height: 100% }
-                    </style>
-                  </head>
-                  <body>
-                    <div id="map"></div>
-                    <script>
-                      mapLink = '<a href="http://openstreetmap.org">OpenStreetMap</a>';
-                      var tiles = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-                      attribution: '&copy; ' + mapLink + ' Contributors'
-                     });
-                """
+                # check if valid GeoJSON
+                if validateJson["type"] == "FeatureCollection":
+                    messageBegin = """
+                    <html>
+                      <head>
+                        <title>ST-Map Preview</title>
+                        <link rel="stylesheet" href="https://unpkg.com/leaflet@1.6.0/dist/leaflet.css"
+                          integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ=="
+                          crossorigin=""/>
+                        <script src="https://unpkg.com/leaflet@1.6.0/dist/leaflet.js"
+                          integrity="sha512-gZwIG9x3wUXg2hdXF6+rVkLF/0Vi9U8D2Ntg4Ga5I5BZpVkVxlJWbSQtXPSiUTtC0TjtGOmxa1AJPuV0CPthew=="
+                          crossorigin=""></script>
+                        <style>
+                          #map{ height: 100% }
+                        </style>
+                      </head>
+                      <body>
+                        <div id="map"></div>
+                        <script>
+                          mapLink = '<a href="http://openstreetmap.org">OpenStreetMap</a>';
+                          var tiles = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+                          attribution: '&copy; ' + mapLink + ' Contributors'
+                         });
+                    """
 
-                geojsonFeature = "    var geojsonFeature = {};".format(viewText)
+                    geojsonFeature = "    var geojsonFeature = {};".format(viewText)
 
-                messageEnd = """
-                      var map = L.map('map');
-                      tiles.addTo(map);
-                      var layerGroup = L.geoJSON(geojsonFeature, {
-                          onEachFeature: function (feature, layer) {
-                              layer.on({
-                                'mouseover': featureProps
-                              });
+                    messageEnd = """
+                          var map = L.map('map');
+                          tiles.addTo(map);
+                          var layerGroup = L.geoJSON(geojsonFeature, {
+                              onEachFeature: function (feature, layer) {
+                                  layer.on({
+                                    'mouseover': featureProps
+                                  });
+                              }
+                          }).addTo(map);
+                          map.fitBounds(L.geoJSON(geojsonFeature).getBounds())
+                          function featureProps(e) {
+                              var layer = e.target;
+                              props = layer.feature.properties;
+                              attrs = Object.keys(props);
+                              var str = '';
+                              for (var i = 0; i < attrs.length; i += 1) {
+                                  attribute = attrs[i];
+                                  value = props[attribute];
+                                  str += '<b>' + attribute + '</b> : ' + value + '<br>'
+                              }
+
+                              layer.bindPopup(str);
                           }
-                      }).addTo(map);
-                      map.fitBounds(L.geoJSON(geojsonFeature).getBounds())
-                      function featureProps(e) {
-                          var layer = e.target;
-                          props = layer.feature.properties;
-                          attrs = Object.keys(props);
-                          var str = '';
-                          for (var i = 0; i < attrs.length; i += 1) {
-                              attribute = attrs[i];
-                              value = props[attribute];
-                              str += '<b>' + attribute + '</b> : ' + value + '<br>'
-                          }
+                        </script>
+                      </body>
+                    </head>
+                    """
 
-                          layer.bindPopup(str);
-                      }
-                    </script>
-                  </body>
-                </head>
-                """
+                    html = messageBegin + geojsonFeature + messageEnd
 
-                html = messageBegin + geojsonFeature + messageEnd
+                    # update output HTML file
+                    tmp_fullpath = get_temp_preview_path(view)
+                    save_utf8(tmp_fullpath, html)
 
-                # update output HTML file
-                tmp_fullpath = get_temp_preview_path(view)
-                save_utf8(tmp_fullpath, html)
-
-                webbrowser.open_new_tab("file:///" + tmp_fullpath)
+                    webbrowser.open_new_tab("file:///" + tmp_fullpath)
+                else:
+                    sublime.error_message("Could not convert file.\n\n Only GeoJSON supported at this time")
             else:
                 sublime.error_message("Could not convert file.\n\n Not valid spatial type")
         except Exception as ex:
